27d61604a0397fd27c0cf59a35e8f165b741626b
Emily Bartman <bartman@nau.edu>
2015-04-02 17:11:50 -0700

    Added student username validation

--- student.php
+++ student.php
@@ -6,15 +6,30 @@ $password = "Killdozer";
 $db = "qwry";
 $link = mysql_connect($server,$username,$password);			
 
-/* Get stuff out of the post array */
-$stud = $_POST["username"];
+/* Get stuff out of the post & get array */
+$username = $_POST["username"];
+$cID = $_GET["classroomID"];
 
 if(!$link){ 
 	die("Could not connect to the CEFNS server: ".mysql_error()); 
 } else {
 	header('Content-Type: text/html; charset=utf-8');
-	$cID = $_GET["classroomID"];
-	
+	mysql_select_db($db, $link);
+	$student_query = mysql_query("SELECT stud FROM student_".$cID." WHERE stud = '".$username."'");
+	if(mysql_num_rows($student_query)){
+		echo "username already exists";
+		header('Location: index.php?error="userexists"');
+		exit();
+	} else {
+		$add_student = "INSERT INTO student_".$cID." VALUES ('".$username."', '".$cID."')";
+		echo $add_student;
+		if(mysql_query($add_student)){
+			echo "created student";
+			//header('Location: student.php');
+			exit();
+		}
+	}
+
 	/******************
 	 * Define Queries *
 	 ******************/
