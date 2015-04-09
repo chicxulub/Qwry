6cae9a0c0d1fe48132011dc25d162236a69d76f2
Emily Bartman <bartman@nau.edu>
2015-04-09 10:26:55 -0700

    Final testing commit

--- js/testing.js
+++ js/testing.js
@@ -1,5 +1,5 @@
 function intro(){
-	/*
+	
 	var intros = $(".test-intro");
 	intros.each(function(index){
 		$(this).delay(1000 + (4000 * index)).show("fade",500).delay(3000).hide("fade",500);
@@ -7,8 +7,8 @@ function intro(){
 	$(".main-container").delay(11000).hide("fade",500);
 	$(".testing").delay(12000).show("fade",1000);
 	$(".test-q").delay(12000).show("fade",1000);
-	*/
+	/*
 	$(".main-container").delay(500).hide("fade",500);
 	$(".testing").delay(510).show("fade",1000)
-	$(".test-q").delay(510).show("fade",1000);
+	$(".test-q").delay(510).show("fade",1000);*/
 }
